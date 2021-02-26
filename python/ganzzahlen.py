318a2ed38d0cc83df3173cd4326b1d1fb0ab2453
agarf <fotografofraga@gmail.com>
2021-02-26 08:42:21 -0300

    inteiros, string, ponto flutuante.

---
+++ python/ganzzahlen.py
@@ -0,0 +1,7 @@
+print(+7)
+print(-10)
+print(7 + 4)
+print(7 - 4)
+print(7 * 4)
+print(3 + 2 *2 -1)
+print((3 + 2) * (2 - 1))
---
+++ python/german.py
@@ -0,0 +1,9 @@
+# Das ist unser Hallo Welt Programm
+
+print("Hallo Welt ich lerne gerade programmieren")
+print("Hallo Welt ich lerne gerade programmieren")
+
+
+print(3 + 2 * 2 - 1)
+print((3 + 2 ) *  (2 - 1))
+print(2.1 + 1.5)
