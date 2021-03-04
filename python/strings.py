b20bc48352329a92df1c9eafc8d6a37e01f0d7a7
agarf <fotografofraga@gmail.com>
2021-03-04 08:15:51 -0300

    strings

---
+++ python/strings.py
@@ -0,0 +1,13 @@
+print("Hallo welt")
+
+name = "Max"
+print("Ich bin:  " + name)
+
+#print("Ich bin:  " + 4)
+
+print("Ich bin:  " + "4")
+
+age = 22
+#print("Ich bin:  " + age)
+
+print("Ich bin:  " + str(age))
