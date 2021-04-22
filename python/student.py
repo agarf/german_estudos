1e6634dfbb029dc1243e52ed07523a62081fd88e
agarf <fotografofraga@gmail.com>
2021-04-22 09:14:24 -0300

    listen

---
+++ python/student.py
@@ -0,0 +1,16 @@
+student1 = "Max"
+student2 = "Monika"
+student3 = "Erik"
+student4 = "Franziska"
+
+students = ["Max", "Monika", "Erik", "Franziska"]
+
+print(students)
+
+students.append("Moritz")
+print(students)
+
+print(len(students))
+print(students[0])
+print(students[4])
+print(students[0] + " & " + students[4])
